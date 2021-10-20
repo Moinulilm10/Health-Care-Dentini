792d92cb2af3ec5f0c52fe533e96e952b78b4fe2
Moinul Islam <moinulilm10@gmail.com>
2021-10-20 21:39:32 +0600

    maybe complete

--- src/Components/Pages/ShowDetails/ShowDetails.js
+++ src/Components/Pages/ShowDetails/ShowDetails.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { Card, Col, Container, Row } from 'react-bootstrap';
+import { Card, Col, Row } from 'react-bootstrap';
 import { useParams } from 'react-router';
 import { Link } from 'react-router-dom';
 import './ShowDetails.css'
@@ -23,16 +23,17 @@ const ShowDetails = () => {
     }, []);
 
 
-    const singleService = showDetails.filter(service => service.id === serviceId);
+    const singleService = showDetails.filter(service => service.id == serviceId);
+
+    console.log(singleService)
 
 
 
     return (
-
         <div>
             <div className="container-fluid  mb-1">
-                <h1 className="heading mb-5 mt-1">
-                    <span className="text-danger"> {singleService[0]?.sName} </span>
+                <h1 className="heading mb-5 mt-5">
+                    <span className="text-dark fw-bold mt-5"> {singleService[0]?.sName} </span>
                 </h1>
                 <Row xs={1} md={2} className="g-3 ">
                     <div className="container-fluid">
@@ -45,7 +46,7 @@ const ShowDetails = () => {
                                         <Card.Title>{singleService[0]?.sName}</Card.Title>
                                         <Card.Text>
                                             <h5>
-                                                About Service: <small className="pb-3 fs-6 text-start"> {singleService[0]?.description}</small>
+                                                About Service: <small className="pb-3 text-start"> {singleService[0]?.description}</small>
                                             </h5>
                                             <p>
                                                 {singleService[0]?.description}
@@ -67,13 +68,7 @@ const ShowDetails = () => {
                 </Row>
             </div>
         </div>
-
-
-
     );
-
-
-
 };
 
 export default ShowDetails;
